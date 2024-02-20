102456081ad8ff100ff4d0d5f15fa3be314d3a48
GabrielCode15 <jorgemagalhaes22.15@gmail.com>
2024-02-20 15:48:05 +0000

    .

---
+++ main.js
@@ -0,0 +1,65 @@
+// elementos
+const alturaInput = document.querySelector("#height");
+const pesoInput = document.querySelector("#weight");
+const calcularBtn = document.querySelector("#calcular");
+const cleanBtn = document.querySelector("#cleanBtn");
+const backBtn = document.querySelector("#backBtn");
+const imcResult = document.querySelector("span");
+imcResult.innerHTML = "";
+
+const formContainer = document.querySelector("#formContainer");
+const resultContainer = document.querySelector("#result-container");
+
+
+function calcularIMC(){
+    let altura = alturaInput.value.replace(",", ".");
+    let peso = pesoInput.value.replace(",", ".");
+    let imcTotal = peso / (altura * altura);
+    
+    if((isNaN(altura) && isNaN(peso)) || (isNaN(altura) && peso) || altura && isNaN(peso)){
+        alert("Introduza números");
+
+    }else if(!altura && !peso){
+        alert("Introduza altura e o peso!");
+
+    }else{
+        if(imcTotal >= 18.5 && imcTotal <= 24.9){
+            imcResult.innerHTML += imcTotal.toFixed(2);
+            imcResult.style.color = "green";
+        }else if(imcTotal >= 25.0 && imcTotal <= 29.9){
+            imcResult.innerHTML += imcTotal.toFixed(2);
+            imcResult.style.color = "orangered";
+        }else if(imcTotal >= 30.0 && imcTotal <= 34.9){
+            imcResult.innerHTML += imcTotal.toFixed(2);
+            imcResult.style.color = "red";
+        }else if(imcTotal >= 35.0 && imcTotal <= 39.9){
+            imcResult.innerHTML += imcTotal.toFixed(2);
+            imcResult.style.color = "red";
+        }else if(imcTotal >= 40.0){
+            imcResult.innerHTML += imcTotal.toFixed(2);
+            imcResult.style.color = "red";
+        }else if(imcTotal < 18.4){
+            imcResult.innerHTML += imcTotal.toFixed(2);
+            imcResult.style.color = "orange";
+        }
+exibirResultado()
+    
+    }
+}
+
+
+calcularBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    calcularIMC();
+})
+
+backBtn.addEventListener("click", ()=>{
+    resultContainer.classList.add("hide");
+    formContainer.classList.remove("hide");
+})
+
+
+function exibirResultado(){
+    formContainer.classList.toggle("hide");
+    resultContainer.classList.toggle("hide");
+}
